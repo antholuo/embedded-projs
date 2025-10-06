0fbe45e8bd5ada6a7215eab4a6344008a811f534
Anthony Luo <anthonyluo100@gmail.com>
2025-10-05 19:40:11 -0700

    smartleds 0.16 works nice with 1.0.0-rc.0

--- esp32_blinky/src/bin/main.rs
+++ esp32_blinky/src/bin/main.rs
@@ -3,8 +3,15 @@
 
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
-use esp_hal::delay::Delay;
+use esp_hal::{delay::Delay,
+rmt::Rmt, time::Rate};
 use esp_hal::main;
+use esp_hal_smartled::{SmartLedsAdapter, smart_led_buffer};
+use smart_leds::{
+    RGB8, SmartLedsWrite, brightness,
+    gamma,
+    hsv::{Hsv, hsv2rgb},
+};
 use log::info;
 
 #[main]
@@ -16,11 +23,32 @@ fn main() -> ! {
 
     esp_println::logger::init_logger_from_env();
 
+    let frequency: Rate = Rate::from_mhz(80);
     // Configure RMT Peripheral
+    let rmt: Rmt<'_, esp_hal::Blocking> = {
+        Rmt::new(_peripherals.RMT, frequency)
+    }.expect("Failed to initialize RMT");
+
+    let rmt_channel = rmt.channel0;
+    let rmt_buffer = smart_led_buffer!(1);
+    let mut led: SmartLedsAdapter<_, 25> = { SmartLedsAdapter::new(rmt_channel, _peripherals.GPIO8, rmt_buffer) };
 
     let delay = Delay::new();
+    let mut color = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
+    let mut data: RGB8;
+    let level = 10;
     loop {
         info!("Hello world!");
+        for hue in 0..=255 {
+            color.hue = hue;
+            data = hsv2rgb(color);
+            led.write(brightness(gamma([data].into_iter()), level)).unwrap();
+            delay.delay_millis(20);
+        }
         delay.delay_millis(500);
     }
 
